f68a94d888fad7e43285ecc2f0bf91d1e11239f5
NicholasJacques <nicholas.d.jacques@gmail.com>
2017-02-16 14:44:30 -0700

    initial commit

---
+++ lib/event_manager.rb
@@ -0,0 +1,10 @@
+require "csv"
+
+puts "EventManager Initialized!"
+
+contents = CSV.open "event_attendees.csv",
+headers: true, header_converters: :symbol
+contents.each do |row|
+  name = row[:first_name]
+  puts name
+end
